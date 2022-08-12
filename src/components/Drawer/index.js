3ff8ac7eb28c3a7745ac977e66d8be0d7afa84c3
sannjayy <znasofficial@gmail.com>
2022-08-12 16:56:11 +0530

    created sidebar

---
+++ src/components/Drawer/index.js
@@ -0,0 +1,78 @@
+import React, { useState, Fragment } from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+import { IconButton } from '@mui/material';
+// import  * as MuiDrawer  from '@mui/material/Drawer';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import Box from '@mui/material/Box';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import PostAddIcon from '@mui/icons-material/PostAdd';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+
+export default function DrawerComp() {
+	const [state, setState] = useState({
+		left: false,
+	})
+	const toggleDrawer = (anchor, open) => (event) => {
+		if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+			return;
+		}
+		setState({ ...state, [anchor]: open });
+	};
+
+	const list = (anchor) => (
+		<Box
+			sx={{ width: 250 }}
+			role="presentation"
+			onClick={toggleDrawer(anchor, false)}
+			onKeyDown={toggleDrawer(anchor, false)}
+		>
+			<List>
+				<ListItem>
+					<ListItemText primary={`ECHO STROKES FORMS`} />
+				</ListItem>
+				<Divider />
+				<ListItem disablePadding>
+					<ListItemButton>
+						<ListItemIcon>
+							<PostAddIcon />
+						</ListItemIcon>
+						<ListItemText primary={`Create a form`} />
+					</ListItemButton>
+				</ListItem>
+			</List>
+			<Divider />
+			<List>
+				<ListItem disablePadding>
+					<ListItemButton>
+						<ListItemIcon>
+							<SettingsIcon />
+						</ListItemIcon>
+						<ListItemText primary={`Settings`} />
+					</ListItemButton>
+				</ListItem>
+			</List>
+		</Box>
+	)
+	return (
+		<div>
+			<Fragment>
+				<IconButton onClick={toggleDrawer('left', true)}>
+					<MenuIcon />
+				</IconButton>
+				<Drawer
+					open={state['left']}
+					onClose={toggleDrawer("left", false)}
+					anchor={`left`}
+				>
+					{list('left')}
+				</Drawer>
+			</Fragment>
+		</div>
+	)
+}
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import './Header.css'
-import MenuIcon from '@mui/icons-material/Menu';
 import { IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import AppsIcon from '@mui/icons-material/Apps';
+import DrawerComp from '../Drawer';
 
 
 export default function Header() {
 	return (
 		<div className="header">
 			<div className='header_info'>
-				<IconButton>
-					<MenuIcon />
-				</IconButton>
+				
+                <DrawerComp />
 				<div className='info'>
 					Echo Strokes
 				</div>
