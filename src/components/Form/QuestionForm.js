77b8e6f912d748e37758ace6ff057670bf107a0d
sannjayy <znasofficial@gmail.com>
2022-08-13 11:33:37 +0530

    designed initial form

--- src/components/Form/QuestionForm.js
+++ src/components/Form/QuestionForm.js
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import './QuestionForm.css'
 import CropOriginalIcon from '@mui/icons-material/CropOriginal';
-import SelectAllIcon from '@mui/icons-material/SelectAll';
+// import SelectAllIcon from '@mui/icons-material/SelectAll';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
-import { FormControlLabel, MenuItem, Select } from '@mui/material';
-
-
+import { FormControlLabel, MenuItem, Select, Button, Switch  } from '@mui/material';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+import SubjectIcon from '@mui/icons-material/Subject';
+import ShortTextIcon from '@mui/icons-material/ShortText';
+import CloseIcon from '@mui/icons-material/Close';
+import { IconButton } from '@mui/material';
+import FilterNoneIcon from '@mui/icons-material/FilterNone';
+import DeleteIcon from '@mui/icons-material/Delete';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import NorthEastIcon from '@mui/icons-material/NorthEast';
 export default function QuestionForm() {
 	const [ questions, setQuestions ] = useState([
 		{
@@ -60,17 +68,78 @@ export default function QuestionForm() {
 						</div>
 					) : ''}					
 				</AccordionSummary>
-				<div className='question-boxes'>
+				<div className='question_boxes'>
 				<AccordionDetails className='add_question'>
-					<div className='add-question-top'>
+					<div className='add_question_top'>
 						<input type="text" className="question" placeholder="Question" value={question.questionText} />
 						<CropOriginalIcon style={{color: '#5f6368'}} />
 						<Select className='select' style={{ color: '#5f6368', fontSize: '13px'}}>
-							<MenuItem id="text" value="Text"> Paragraph </MenuItem>
-							<MenuItem id="checkbox" value="Checkbox"> Checkbox </MenuItem>
-							<MenuItem id="radio" value="Radio"> Multiple Choice </MenuItem>
+							<MenuItem id="text" value="Text">
+                                <SubjectIcon style={{ marginRight: '10px'}} />
+                                Paragraph 
+                            </MenuItem>
+							<MenuItem id="checkbox" value="Checkbox">
+                                <CheckBoxIcon style={{ marginRight: '10px'}} />
+                                Checkbox 
+                            </MenuItem>
+							<MenuItem id="radio" value="Radio"> 
+                                <RadioButtonCheckedIcon style={{ marginRight: '10px'}} />
+                                Multiple Choice 
+                            </MenuItem>
 						</Select>
 					</div>
+                    {question.options.map((option, j) => (
+                        <div className="add_question_body" key={j}>
+                            {
+                                (question.questionType !== 'text') ?
+                                <input type={question.questionType} style={{ marginRight: '10px' }} /> :
+                                <ShortTextIcon style={{marginRight: '10px' }} />
+                            }
+                            <div>
+                                <input type='text' className='text_input' placeholder='option' value={question.options[j].optionText} />
+                            </div>
+                            
+                            <CropOriginalIcon />
+
+                            <IconButton aria-label="delete">
+                                <CloseIcon />
+                            </IconButton>
+                        </div>
+                    ))}
+                    { question.options.length < 5 ? (
+                        <div className='add_question_body'>
+                            <FormControlLabel disabled control={
+                                (question.questionType !== 'text') ? 
+                                <input type={question.questionType} color="primary" inputProps={{ 'aria-label': 'secondary checkbox'}} style={{marginLeft: '10px', marginRight: '10px' }} disabled /> :
+                                <ShortTextIcon style={{marginRight: '10px' }} />
+                            } label={
+                                <div>
+                                    <input type='text' className="text_input" style={{fontSize:'13px', width:'60px'}} placeholder="add other" />
+                                    <Button siz e="small" style={{textTransform: 'none', color: "#4285f4", fontSize:'13px', fontWeight: '600'}}>Add Option</Button>
+                                </div>
+                            } />
+                        </div>
+                    ) : ''}
+                    <div className="add_footer">
+                        <div className="add_question_bottom_left">
+                            <Button size='small' style={{ textTransform: 'none', color: '#4285f4', fontSize: '13px', fontWeight: '600' }}>
+                                <NorthEastIcon />
+                                Answer Key
+                            </Button>
+                        </div>
+                        <div className="add_question_bottom">
+                            <IconButton aria-label="copy">
+                                <FilterNoneIcon />
+                            </IconButton>
+                            <IconButton aria-label="delete">
+                                <DeleteIcon />
+                            </IconButton>
+                            <span>Required <Switch name="checkedA" color="primary" /></span>
+                            <IconButton>
+                                <MoreVertIcon />
+                            </IconButton>
+                        </div>
+                    </div>
 				</AccordionDetails>
 				</div>
 			</Accordion>
